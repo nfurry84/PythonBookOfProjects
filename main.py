3ba7f3bf3a7bae09db71526331cbdb11359ee432
Nick Furry <61699144+nfurry84@users.noreply.github.com>
2024-10-12 00:57:22 -0400

    Update main.py
    Add Config and Secret variables

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 """This is a hello world style script"""
 def print_hi(name):
     """print hi says hello to input parameter"""
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi there, {name}')
+    conf = os.environ.get('MY_GLOBAL_APP_VARIABLE')
+    secret_value = os.environ.get('MY_SECRET')
+    print(f'Config: {conf}')
+    print(f'Secret: {secret_value}')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Foo user')
