4a75656af3214c20f1ea438f48d4e4dfcd05370d
Nick Furry <61699144+nfurry84@users.noreply.github.com>
2024-10-12 01:12:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def print_hi(name):
     """print hi says hello to input parameter"""
     print(f'Hi there, {name}')
     conf = os.environ.get('MY_GLOBAL_APP_VARIABLE') #osnow
-    secret_value = os.environ.get('MY_SECRET')
+    not_secret_value = os.environ.get('MS1234')
     print(f'Config: {conf}')
-    print(f'Secret: {secret_value}')
+    print(f'Secret: {not_secret_value}')
 
 
 if __name__ == '__main__':
