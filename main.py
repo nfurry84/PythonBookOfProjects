280ab2f4a175be06267c56c9e2a62504b31972b0
Nick Furry <nickfurry84@gmail.com>
2024-10-12 15:39:59 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 """This is a hello world style script"""
 import os
-conf = ''
-key = ''
+CONF = ''
+KEY = ''
 
 
 def print_hi(name):
     """print hi says hello to input parameter"""
     print(f'Hi there, {name}')
-    conf = os.environ.get('MY_GLOBAL_APP_VARIABLE') #osnow
-    key = os.environ.get('MY_SECRET')
+    CONF = os.environ.get('MY_GLOBAL_APP_VARIABLE') #osnow
+    KEY = os.environ.get('MY_SECRET')
     
-    
+    dowork(conf,key)
     #print(f'Config: {conf}')
     #print(f'Secret: {not_secret_value}')
 
+def dowork(x,y):
+    print(f'{x} : {y}')
 
 if __name__ == '__main__':
     print_hi('Foo user')
